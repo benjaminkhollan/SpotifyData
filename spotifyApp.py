ec11c178dc82e0dcdb45c2252396d5302d0e4737
benjaminkhollan <benjamin.holland99@gmail.com>
2024-08-10 09:00:27 -0700

    spotify data streamlit cont

--- spotifyApp.py
+++ spotifyApp.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import json
+import datetime
 
 
 st.title("Hello, Streamlit!")
@@ -35,13 +36,30 @@ for i in range(len(file_names)):
   dfs.append(df)
 combined_df = pd.concat(dfs, ignore_index=True)
 
-min_time = combined_df["ts"].min()
-max_time = combined_df["ts"].max()
+combined_df['timestamp'] = pd.to_datetime(combined_df['ts'])
+combined_df['timestamp'] =pd.to_datetime(combined_df['timestamp'].dt.floor('d')).dt.strftime('%Y-%m-%d')
+
+
+#combined_df['timestamp'] = datetime.datetime.fromtimestamp(combined_df['ts'])
+
+#pd.to_datetime(combined_df['ts'].str.replace(r'\[.*\]', '', regex=True),format='ISO8601')
+
+#v = datetime.datetime.fromtimestamp(v)
+
+min_time = datetime.strptime(combined_df["timestamp"].min(), '%Y-%m-%d').date()
+max_time = datetime.strptime(combined_df["timestamp"].max(), '%Y-%m-%d').date()
+
+#min_time = pd.to_datetime(combined_df["timestamp"].min())
+#max_time = pd.to_datetime(combined_df["timestamp"].max())
+
+print(type(min_time))
+print(type(max_time))
+
 st.write(f"From {min_time} to {max_time}")
 
 filter_value = st.slider('Select a threshold for x', min_value=min_time, max_value=max_time, value=min_time)
 
-filtered_df = combined_df[combined_df['ts'] > filter_value]
+filtered_df = combined_df[combined_df['timestamp'] > filter_value]
 
 st.dataframe(combined_df)
 
