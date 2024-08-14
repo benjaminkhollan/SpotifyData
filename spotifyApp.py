6511a0a3eeee56906803e95f9ea155eba33ed685
benjaminkhollan <benjamin.holland99@gmail.com>
2024-08-13 21:36:26 -0700

    spotify data streamlit cont

--- spotifyApp.py
+++ spotifyApp.py
@@ -37,7 +37,10 @@ for i in range(len(file_names)):
 combined_df = pd.concat(dfs, ignore_index=True)
 
 combined_df['timestamp'] = pd.to_datetime(combined_df['ts'])
-combined_df['timestamp'] =pd.to_datetime(combined_df['timestamp'].dt.floor('d')).dt.strftime('%Y-%m-%d')
+combined_df['timestamp'] = pd.to_datetime(combined_df['timestamp'].dt.floor('d')).dt.strftime('%Y-%m-%d')
+combined_df['timestamp'] = combined_df['timestamp'].dt.date
+#combined_df['timestamp'] = datetime.datetime.strptime(combined_df["timestamp"], '%Y-%m-%d').date()
+#combined_df['timestamp'] = pd.to_datetime(combined_df['timestamp'])
 
 
 #combined_df['timestamp'] = datetime.datetime.fromtimestamp(combined_df['ts'])
@@ -46,8 +49,8 @@ combined_df['timestamp'] =pd.to_datetime(combined_df['timestamp'].dt.floor('d'))
 
 #v = datetime.datetime.fromtimestamp(v)
 
-min_time = datetime.strptime(combined_df["timestamp"].min(), '%Y-%m-%d').date()
-max_time = datetime.strptime(combined_df["timestamp"].max(), '%Y-%m-%d').date()
+min_time = datetime.datetime.strptime(combined_df["timestamp"].min(), '%Y-%m-%d').date()
+max_time = datetime.datetime.strptime(combined_df["timestamp"].max(), '%Y-%m-%d').date()
 
 #min_time = pd.to_datetime(combined_df["timestamp"].min())
 #max_time = pd.to_datetime(combined_df["timestamp"].max())
@@ -58,6 +61,7 @@ print(type(max_time))
 st.write(f"From {min_time} to {max_time}")
 
 filter_value = st.slider('Select a threshold for x', min_value=min_time, max_value=max_time, value=min_time)
+print(type(filter_value))
 
 filtered_df = combined_df[combined_df['timestamp'] > filter_value]
 
